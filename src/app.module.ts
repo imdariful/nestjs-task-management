e571c18ba162e68a3e532002c418cf80a7fdfd88
Ariful Islam <ariful.rony10@gmail.com>
2023-10-16 14:08:46 +0600

    connection to db and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '8520',
+      database: 'task-management',
+      autoLoadEntities: true, // find entity file and load them
+      synchronize: true, // always keep database schema on sync
+    }),
+  ],
   controllers: [],
   providers: [],
 })
